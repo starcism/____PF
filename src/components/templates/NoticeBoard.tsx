d93948e64d55bba119031afe6632d6ee2dd73453
sangmoon <laugh120@naver.com>
2023-09-28 20:24:08 +0900

    set-notice

--- src/components/templates/NoticeBoard.tsx
+++ src/components/templates/NoticeBoard.tsx
@@ -5,9 +5,8 @@ export default function NoticeBoard() {
     <>
       <div className="p-[50px] w-full h-full">
         <p className="text-[14px] leading-[24px]">
-          <h2>lovedive.net 개발자입니다.</h2>
           <h2>개발은 진행중이고, 테스트 목적으로 사이트를 배포해두었습니다.</h2>
-          <h2>모든 피드백은 laugh120@gmail.com로 메일을 보내주시거나,</h2>
+          <h2>모든 피드백은 메뉴에서 문의를 거쳐 메일을 보내주시거나,</h2>
           <h2>포럼 게시판에 글 남겨주시면 적극적으로 수용하겠습니다.</h2>
         </p>
       </div>
